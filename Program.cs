d9420a12c0aa5dc6460165c35502e6628f23e5a9
ShrineFox <ShrineFox@users.noreply.github.com>
2024-07-24 20:41:45 -0400

    Initial commit
    Works with ACB files that don't contain a mix of streaming and non-streaming audio

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+// Using code from: https://github.com/DarkPsydeOfTheMoon/EVTUI
+namespace ACBCueConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                Console.WriteLine("Outputs a TSV of Cue names to the directory of an ACB File." +
+                    "\nUsage: ACBCueConverter.exe \"<PathToAcbFile>\"" +
+                    "\nOutput: awb_id,cue_name" +
+                    "\n\nPress any key to exit.");
+                Console.ReadKey();
+                return;
+            }
+
+            string sourceAcb = args[0];
+
+            ACB acb = new ACB(sourceAcb, new AudioCues() { });
+            if (acb.TrackList == null)
+            {
+                Console.WriteLine("Unable to read TrackList from ACB, TrackList object is null.");
+                return;
+            }
+
+            Console.WriteLine();
+            string outTsvLines = "";
+            foreach (var track in acb.TrackList.OrderBy(x => x.AwbId))
+            {
+                if (!outTsvLines.Contains($"\n{track.AwbId}\t"))
+                {
+                    string newLine = $"\n{track.AwbId}\t{track.CueName}";
+                    Console.WriteLine(newLine);
+                    outTsvLines += newLine;
+                }
+                else
+                {
+                    string newLine = $",{track.CueName}";
+                    Console.WriteLine(newLine);
+                    outTsvLines += newLine;
+                }
+            }
+
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(sourceAcb), 
+                Path.GetFileNameWithoutExtension(sourceAcb) + ".tsv"), outTsvLines);
+        }
+    }
+}
