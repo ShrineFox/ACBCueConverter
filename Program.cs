dfa9a78ddc04baf2a777b1f43fbb7168cc198faf
ShrineFox <ShrineFox@users.noreply.github.com>
2024-07-25 01:32:28 -0400

    Add commandline args, handle moving files for Ryo
    I don't think the cue names are matched properly to IDs yet.

--- Program.cs
+++ Program.cs
@@ -2,57 +2,112 @@
 using V3Lib.CriWare;
 using ShrineFox.IO;
 using System.Collections.ObjectModel;
+using TGE.SimpleCommandLine;
+using static System.Windows.Forms.Design.AxImporter;
+using System.Windows.Forms;
 
 // Using code from: https://github.com/DarkPsydeOfTheMoon/EVTUI
 namespace ACBCueConverter
 {
     internal class Program
     {
+        public static ProgramOptions options;
+
+        public class ProgramOptions
+        {
+            [Option("a", "acbPath", "acb path", "Specifies the path to the ACB file to get Cue Names and AWB IDs from.", Required = true)]
+            public string AcbPath { get; set; } = "";
+            [Option("i", "inputDir", "directory path", "Specifies the path to the directory with FEmulator .adx to copy.", Required = true)]
+            public string InputDir { get; set; } = "";
+            [Option("o", "outDir", "directory path", "Specifies the path to the Ryo ACB directory to output .adx to.", Required = true)]
+            public string OutDir { get; set; } = "";
+            [Option("f", "fallbackTsv", "tsv path", "Specifies the path to the .txt or .tsv to use for mapping AWB IDs to Cue Names.")]
+            public string FallbackTsv { get; set; } = "";
+            [Option("d", "debug", "boolean", "If true, TSVs for processed ACB files will be output next to their locations.")]
+            public bool Debug { get; set; } = false;
+        }
+
         static void Main(string[] args)
         {
+            string about = SimpleCommandLineFormatter.Default.FormatAbout<ProgramOptions>("ShrineFox",
+                "Outputs ADX files from AWBEmulator format to Ryo format based on a TSV file.");
+            try
+            {
+                options = SimpleCommandLineParser.Default.Parse<ProgramOptions>(args);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.WriteLine(about);
+
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            if (args.Length == 0 || !File.Exists(args[0]))
-            {
-                ShowUsageInfo();
-                return;
-            }
+            var nameMap = GetNameMapFromACB();
+            var awbMap = GetAwbMapFromACB();
 
-            string acbPath = args[0];
-            string awbPath = FileSys.GetExtensionlessPath(acbPath) + ".awb";
-            if (File.Exists(awbPath))
-                WriteTSVFromACB(acbPath, awbPath);
-            else
-                WriteTSVFromACB(acbPath);
+            CopyFilesToNewDestination(nameMap, awbMap);
         }
 
-        public static void WriteTSVFromACB(string acbPath, string awbPath)
+        private static void CopyFilesToNewDestination(List<CueInfo> nameMap, List<CueInfo> awbMap)
+        {
+            foreach (var adx in Directory.GetFiles(options.InputDir, "*.adx", SearchOption.TopDirectoryOnly))
+            {
+                int adxId = Convert.ToInt32(Path.GetFileNameWithoutExtension(adx).Split('_')[0]);
+                if (awbMap.Any(x => x.AwbId == adxId))
+                {
+                    foreach (var cueName in awbMap.First(x => x.AwbId == adxId).CueNames)
+                    {
+                        int cueNameId = nameMap.First(x => x.CueNames.Any(x => x.Equals(cueName))).AwbId;
+
+                        string cueDir = Path.Combine(options.OutDir, cueNameId + ".cue");
+                        Directory.CreateDirectory(cueDir);
+                        string outFile = Path.Combine(cueDir, Path.GetFileName(adx));
+                        File.Copy(adx, outFile, true);
+                        string outFileConfigPath = Path.Combine(cueDir, Path.GetFileNameWithoutExtension(adx) + ".yaml");
+                        File.WriteAllText(outFileConfigPath, "player_id: -1\nvolume: 1.0");
+                    }
+                }
+            }
+        }
+
+        public static List<CueInfo> GetNameMapFromACB()
         {
             AcbFile loadedAcb = new AcbFile();
-            loadedAcb.Load(acbPath);
+            loadedAcb.Load(options.AcbPath);
 
-            AwbFile loadedAwb = new AwbFile();
-            loadedAwb.Load(awbPath);
+            var awbIDCueNamePairs = GetAwbIDCueNamePairs(loadedAcb.Cues);
 
-            var awbIdCueNamePairs = GetAwbIDCueNamePairs(loadedAcb.Cues);
-            var audio = loadedAwb.AudioData;
+            if (options.Debug)
+            {
+                string outTsvPath = FileSys.GetExtensionlessPath(options.AcbPath) + "_nameMap.tsv";
+                WriteTSV(awbIDCueNamePairs, outTsvPath);
+            }
 
-            string outTsvPath = FileSys.GetExtensionlessPath(acbPath) + ".tsv";
-            WriteTSV(awbIdCueNamePairs, outTsvPath);
+            return awbIDCueNamePairs;
         }
 
-        public static void WriteTSVFromACB(string acbPath)
+        public static List<CueInfo> GetAwbMapFromACB()
         {
-            ACB acb = new ACB(acbPath, new AudioCues() { });
+            ACB acb = new ACB(options.AcbPath, new AudioCues() { });
             if (acb.TrackList == null)
             {
-                Console.WriteLine("Unable to read TrackList from ACB, TrackList object is null.");
-                return;
+                Console.WriteLine($"Unable to read TrackList from ACB, using fallback: {Path.GetFileName(options.AcbPath)}");
+                
+                return GetCueListFromTsv(options.FallbackTsv);
             }
-            var awbIdCueNamePairs = GetAwbIDCueNamePairs(acb.TrackList);
-            string outTsvPath = FileSys.GetExtensionlessPath(acbPath) + ".tsv";
+            var awbMap = GetAwbIDCueNamePairs(acb.TrackList);
 
-            WriteTSV(awbIdCueNamePairs, outTsvPath);
+            if (options.Debug)
+            {
+                string outTsvPath = FileSys.GetExtensionlessPath(options.AcbPath) + "_awbMap.tsv";
+                WriteTSV(awbMap, outTsvPath);
+            }
+
+            return awbMap;
+
         }
 
         public static void WriteTSV(List<CueInfo> awbIdCueNamePairs, string outTsvPath)
@@ -62,7 +117,7 @@ namespace ACBCueConverter
             string outTsvLines = "";
             foreach (var idPair in awbIdCueNamePairs)
             {
-                string newLine = $"\n{idPair.AwbId}\t{string.Join(',', idPair.CueNames)}";
+                string newLine = $"\n{idPair.AwbId}\t{string.Join(';', idPair.CueNames)}";
                 Console.WriteLine(newLine);
                 outTsvLines += newLine;
             }
@@ -100,13 +155,24 @@ namespace ACBCueConverter
             return awbIdCueNamePairs;
         }
 
-        public static void ShowUsageInfo()
+        private static List<CueInfo> GetCueListFromTsv(string tsvPath)
         {
-            Console.WriteLine("Outputs a TSV of Cue names to the directory of an ACB File." +
-                    "\nUsage: ACBCueConverter.exe \"<PathToAcbFile>\"" +
-                    "\nOutput: awb_id,cue_name" +
-                    "\n\nPress any key to exit.");
-            Console.ReadKey();
+            List<CueInfo> cues = new List<CueInfo>();
+
+            var lines = File.ReadAllLines(tsvPath);
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                string line = lines[i].TrimEnd(')').Replace(" (", "\t").Replace("; ", ";");
+
+                CueInfo cue = new CueInfo();
+
+                cue.AwbId = i;
+                cue.CueNames = line.Split('\t')[1].Split(';').ToList();
+
+                cues.Add(cue);
+            }
+
+            return cues;
         }
 
         public class CueInfo
